63614f831224c7f11b55c4b1fd771e3fe8637a44
RISHIKESH SARODE <60572344+rishi-99@users.noreply.github.com>
2020-03-16 22:18:45 +0530

    Update funbox.py

--- funbox.py
+++ funbox.py
@@ -4,80 +4,121 @@ from  random import *
 
 
 class blob:
-    def __init__(self,r,thickness,spawn,speed,limit):
-        self.x = spawn[0]
-        self.y = spawn[1]
-        self.xlimit = (r+thickness,limit[0]-(r+thickness))
-        self.ylimit = (r+thickness,limit[1]-(r+thickness))
-        self.position = (self.x, self.y)
-        self.radius = r
-        self.thickness = thickness
-        self.speed_x =speed[0]*choice([-1,1])
-        self.speed_y =speed[1]*choice([-1,1])
+	def __init__(self,r,thickness,spawn,speed,limit):
+		self.x = spawn[0]
+		self.y = spawn[1]
+		self.xlimit = (r+thickness,limit[0]-(r+thickness))
+		self.ylimit = (r+thickness,limit[1]-(r+thickness))
+		self.position = (self.x, self.y)
+		self.radius = r
+		self.thickness = thickness
+		self.speed_x =speed[0]*choice([-1,1])
+		self.speed_y =speed[1]*choice([-1,1])
+		self.infected = False
+		self.immunizer = False
+		self.color = [0,255,0]
 
 
+	def update(self):
 
-    def update(self):
+		# Bounce back from the edges by reversing the spped of x and y
+		if self.x <= self.xlimit[0] or self.x >=self.xlimit[1]:
+			self.speed_x  = self.speed_x*-1
+		if self.y <= self.ylimit[0] or self.y >= self.ylimit[1]:
+			self.speed_y  = self.speed_y*-1
 
-        # Bounce back from the edges by reversing the spped of x and y
-        if self.x <= self.xlimit[0] or self.x >=self.xlimit[1]:
-            self.speed_x  = self.speed_x*-1
-        if self.y <= self.ylimit[0] or self.y >= self.ylimit[1]:
-            self.speed_y  = self.speed_y*-1
+		# update the co-ordinates
+		self.x += self.speed_x
+		self.y += self.speed_y
+		self.position  = (self.x,self.y)
+		if self.infected:
+			self.color = [0,0,255]
+		elif self.immunizer:
+			self.color= [255,0,0]
+		else:
+			self.colo = [0,255,0]
 
-        # update the co-ordinates
-        self.x += self.speed_x
-        self.y += self.speed_y
-        self.position  = (self.x,self.y)
+	def __sub__(self, other):
+		# returns the distance from other blob
 
-    def __sub__(self, other):
-        # returns the distance from other blob
-
-        return int( ((self.x-other.x)**2 + (self.y-other.y)**2 )**(1/2))
+		return int( ((self.x-other.x)**2 + (self.y-other.y)**2 )**(1/2))
 
 
 
 if "__main__" == __name__:
 
-    #-- Setting of window size and blob features-----
+	#-- Setting of window size and blob features-----
 
-    window = (700,700)
-    blob_color = [255, 255, 255]
-    # bg_color = [213, 193,62 ]
-    bg_color = [0, 0,0 ]
-    bob_sensitive_dist = 300
-    speed = 20
-    no_blob = 30
-
-    #-------------------------------------------------
-
-
-    blobs=  []
-    for x in range(no_blob):
-        blobs.append(blob(5,1, (randint(0,window[0]),randint(0,window[1])),(randint(1,speed),randint(1,speed)),window))
+	window = (1200,1000)
+	line_color = [0, 255, 0]
+	# bg_color = [213, 193,62 ]
+	bg_color = [0, 0,0 ]
+	bob_sensitive_dist = 100
+	speed = 20
+	no_blob = 60
+	sensitivity = 0.80
+	#-------------------------------------------------
 
 
-    while True:
+	blobs=  []
+	for x in range(no_blob):
+		blobs.append(blob(5,1, (randint(0,window[0]),randint(0,window[1])),(randint(1,speed),randint(1,speed)),window))
+	blobs[1].infected = True  #introduce infection
+	blobs[2].immunizer = True  #introduce immunizer
+	blobs[3].immunizer = True  #introduce immunizer
+	blobs[4].immunizer = True  #introduce immunizer
 
-        # color = [213, 193,62 ]
-        main_matrix = np.array( [[bg_color]*window[0]]*window[1], np.uint8)
-        # main_matrix = np.zeros((window[0],window[1]),np.uint8)
+	while True:
 
-        for Blob in blobs:
-            for other in blobs:
+		# color = [213, 193,62 ]
+		main_matrix = np.array( [[bg_color]*window[0]]*window[1], np.uint8)
+		# main_matrix = np.zeros((window[0],window[1]),np.uint8)
 
-                if Blob !=other:
-                    if 0 < Blob-other  < bob_sensitive_dist:
-                        ratio = (Blob - other) / bob_sensitive_dist
-                        brightB =  bg_color[0]*(ratio) + blob_color[0]*(1-ratio)
-                        brightG =  bg_color[1]*(ratio) + blob_color[1]*(1-ratio)
-                        brightR =  bg_color[2]*(ratio) + blob_color[2]*(1-ratio)
-                        cv2.line(main_matrix, (Blob.x,Blob.y), (other.x,other.y), (brightB,brightG ,brightR), 1)
+		for Blob in blobs:
+			for other in blobs:
 
-        for Blob in blobs:
-            cv2.circle(main_matrix, Blob.position, Blob.radius, (blob_color[0], blob_color[1], blob_color[2]), -1)
-            Blob.update()
-        cv2.imshow("image", main_matrix)
+				if Blob !=other:
+					if 0 < Blob-other  < bob_sensitive_dist:
+						if not other.infected and not Blob.infected:
+								continue
+						if not other.infected and Blob.infected:
+							if Blob-other <=  bob_sensitive_dist*sensitivity:
+								if not Blob.infected:
+										if Blob.immunizer:
+											if other.infected:
+												other.infected = False
+										else:
+											if other.infected:
+												Blob.infected = True
+												
+										
+											
+								elif Blob.infected:
+										if not other.infected:
+											if other.immunizer:
+												Blob.infected = False
+											else:
+												other.infected = True
+										elif other.immunizer:
+											other.infected = False
 
-        if cv2.waitKey(200) & 0xFF == ord('q'):
-            break
+								# elif Blob.infected and not other.infected:
+									# other.infected = True								
+									
+						
+						
+						
+						ratio = (Blob - other) / bob_sensitive_dist
+						brightB =  bg_color[0]*(ratio) + line_color[0]*(1-ratio)
+						brightG =  bg_color[1]*(ratio) + line_color[1]*(1-ratio)
+						brightR =  bg_color[2]*(ratio) + line_color[2]*(1-ratio)
+						cv2.line(main_matrix, (Blob.x,Blob.y), (other.x,other.y), (brightB,brightG ,brightR), 1)
+
+		for Blob in blobs:
+			blob_color = Blob.color
+			cv2.circle(main_matrix, Blob.position, Blob.radius, (blob_color[0], blob_color[1], blob_color[2]), -1)
+			Blob.update()
+		cv2.imshow("image", main_matrix)
+
+		if cv2.waitKey(200) & 0xFF == ord('q'):
+			break
